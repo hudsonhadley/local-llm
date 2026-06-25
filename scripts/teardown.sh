3c94105082572a1c0876912bc3779880ea53ff3a
hudsonhadley <hudsonhadley@yahoo.com>
2026-06-25 11:21:02 -0400

    Adding copy of example doc to documents as part of teardown

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+
+cd "$(dirname "$0")"
 
 echo "Stopping docker containers and volumes..."
 docker compose -f ../docker/docker-compose.yml down -v
@@ -9,5 +12,8 @@ rm -rf ../config/*
 rm -rf ../keys/*
 rm -rf ../flows/runtime/*
 rm -rf ../documents/*
+
+echo "Copying inject document to documents dir..."
+cp ../documents.example/* ../documents/
 
 echo "Done."
