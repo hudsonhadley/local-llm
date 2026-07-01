cc8c107c188ec2534e0b53ce9247bc96091c7d2b
hudsonhadley <hudsonhadley@yahoo.com>
2026-07-01 09:34:40 -0400

    Setting up main script

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+_script_dir = os.path.dirname(os.path.abspath(__file__))
+_root_dir = os.path.dirname(_script_dir)
+load_dotenv(os.path.join(_root_dir, ".env"))
+
 def main():
-    print("Hello from localllm!")
+    print("Hello I am running")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
